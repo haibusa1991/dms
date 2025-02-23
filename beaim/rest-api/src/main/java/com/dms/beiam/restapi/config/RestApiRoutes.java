d2f7ea1bb518edddb1f26e367bb103351bf7c8f7
Plamen Simeonov <61594367+haibusa1991@users.noreply.github.com>
2025-02-23 14:35:28 +0200

    Delete beaim directory

--- beaim/rest-api/src/main/java/com/dms/beiam/restapi/config/RestApiRoutes.java
+++
@@ -1,6 +0,0 @@
-package com.dms.beiam.restapi.config;
-
-public class RestApiRoutes {
-    public static final String API_V1 = "/api/v1";
-    public static final String REGISTER_IDENTITY = API_V1 + "/register";
-}
