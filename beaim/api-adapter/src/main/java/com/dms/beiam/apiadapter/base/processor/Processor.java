a65bf44d8669daedf1132d5c040d40e978f2b3cd
Plamen Simeonov <61594367+haibusa1991@users.noreply.github.com>
2025-02-23 14:35:13 +0200

    Delete beaim directory

--- beaim/api-adapter/src/main/java/com/dms/beiam/apiadapter/base/processor/Processor.java
+++
@@ -1,9 +0,0 @@
-package com.dms.beiam.apiadapter.base.processor;
-
-
-import com.dms.beiam.apiadapter.base.error.ProcessorError;
-import io.vavr.control.Either;
-
-public interface Processor<R extends ProcessorResult, I extends ProcessorInput> {
-    Either<ProcessorError, R> process(I input);
-}
--- beaim/api-adapter/src/main/java/com/dms/beiam/apiadapter/operations/v1/register/RegisterIdentity.java
+++
@@ -1,7 +0,0 @@
-package com.dms.beiam.apiadapter.operations.v1.register;
-
-import com.dms.beiam.apiadapter.base.processor.Processor;
-
-public interface RegisterIdentity extends Processor<BeiamRegisterIdentityResult, BeiamRegisterIdentityInput> {
-
-}
